56fe00c5259edc308f413285d85c975882bdc1d5
GoGradually <chkun3109@gmail.com>
2025-12-07 16:23:57 +0900

    feat: tutorial 1

---
+++ src/main/java/me/rabbitmqserver1/RabbitAmqpTutorialsRunner.java
@@ -0,0 +1,21 @@
+package me.rabbitmqserver1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class RabbitAmqpTutorialsRunner implements CommandLineRunner {
+    @Value("${tutorial.client.duration:0}")
+    public int duration;
+
+    @Autowired
+    private ConfigurableApplicationContext ctx;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Ready ... running for " + duration + "ms");
+        Thread.sleep(duration);
+        ctx.close();
+    }
+}
---
+++ src/test/java/me/rabbitmqserver1/RabbitMqServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.rabbitmqserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqServer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
